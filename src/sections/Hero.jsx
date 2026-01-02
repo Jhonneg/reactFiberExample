4cc9b5cffd72c3344419edf1601016cdde1838fc
Jhonne <jonee4339@hotmail.com>
2026-01-02 15:51:48 -0300

    added extra markup and constants file

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,4 @@
+import { words } from "../constants/index";
 
 export default function Hero() {
   return (
@@ -5,6 +6,43 @@ export default function Hero() {
       <div className="absolute top-0 left-0 z-10">
         <img src="/images/bg.png" alt="background" />
         Hero
+      </div>
+      <div className="hero-layout">
+        <header
+          className="flex flex-col justify-center 
+        wd:w-full w-screen md:px-20 px-5"
+        >
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center mg:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
+            </div>
+            <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+              Hi i'm john doe, a dev based in with a passion for code
+            </p>
+          </div>
+        </header>
       </div>
     </section>
   );
